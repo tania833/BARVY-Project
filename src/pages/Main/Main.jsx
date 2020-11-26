d4376ab550087739fd2b211ae6a454bc7416a89c
Tania Polishchuk <tania.polischuk1998@gmail.com>
2020-11-26 21:19:20 +0200

    delete empty lines

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -6,7 +6,7 @@ import ArtistsMain from "./components/ArtistsMain/ArtistsMain";
 import FooterMainBlue from "./components/FooterMainBlue/FooterMainBlue";
 import HowItWorks from "./components/HowItWorks/HowItWorks";
 import Technologies from "./components/Technologies/Technologies";
-import LearnAndChoose from './components/LearnAndChoose/LearnAndChoose';
+import LearnAndChoose from "./components/LearnAndChoose/LearnAndChoose";
 
 const Main = () => {
   return (
