6fa6f0006ee611dd1cf4f53ba2f0ff0e3e541071
SvitlanaKalmutska <svitlachok.k@gmail.com>
2020-12-03 13:10:16 +0200

    created mock-file

---
+++ src/MOCKS/our_teachers_info_MOCK.js
@@ -0,0 +1,104 @@
+const OurTeachers [
+ {
+     id:1,
+     img: {
+         url: "https://i.imgur.com/nY2JpK0.png",
+         alt: "Khrystyna_Strynadyuk",
+     },
+     teacher: {
+         name: "Христина Стринадюк",
+         technique: "Акварель, офорт, ілюстрація",
+     },
+ },
+
+ {
+    id:2,
+    img: {
+        url: "https://i.imgur.com/8zzEEkV.jpg",
+        alt: "Pavlo_Benedyuk",
+    },
+    teacher: {
+        name: "Павло Бенедюк",
+        technique: "Академічний рисунок, скетчинг, акварель",
+    },
+},
+{
+    id:3,
+    img: {
+        url: "https://i.imgur.com/fHsKJ66.png",
+        alt: "Natalia_Brichuk",
+    },
+    teacher: {
+        name: "Наталя Брічук",
+        technique: "Графіка, фотографія",
+    },
+},
+{
+    id:4,
+    img: {
+        url: "https://i.imgur.com/o7K9Wk2.png",
+        alt: "Stepan_Karpyuk",
+    },
+    teacher: {
+        name: "Степан Карпюк",
+        technique: "Акварель",
+    },
+},
+{
+    id:5,
+    img: {
+        url: "https://i.imgur.com/ZsHo1OM.png",
+        alt: "Oleksandra_Promska",
+    },
+    teacher: {
+        name: "Олександра Промська",
+        technique: "Ілюстрація, колаж",
+    },
+},
+{
+    id:6,
+    img: {
+        url: "https://i.imgur.com/tRcgha5.png",
+        alt: "Natalia_Servetnyk",
+    },
+    teacher: {
+        name: "Наталя Серветник",
+        technique: "Акварель, графіка",
+    },
+},
+{
+    id:7,
+    img: {
+        url: "https://i.imgur.com/GNJMt1t.png",
+        alt: "Carolina_Bundash",
+    },
+    teacher: {
+        name: "Кароліна Бундаш",
+        technique: "Акварель",
+    },
+},
+{
+    id:8,
+    img: {
+        url: "https://i.imgur.com/rYUWS6l.png",
+        alt: "Olexander_Bobryshev",
+    },
+    teacher: {
+        name: "Олександр Бобришев",
+        technique: "Акварель",
+    },
+},
+{
+    id:9,
+    img: {
+        url: "https://i.imgur.com/FxSvvrB.png",
+        alt: "Anton_Filyk",
+    },
+    teacher: {
+        name: "Антон Філик",
+        technique: "Ілюстрація, скетчинг",
+    },
+},  
+]
+
+export default OurTeachers;
