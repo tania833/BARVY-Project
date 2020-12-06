523874bc53310a9b5863982bb4ab52c40194c186
Towko <antonyo0182@gmail.com>
2020-12-06 20:05:12 +0200

    upd to adding by props blocks for technologies

---
+++ src/MOCKS/techArtCard_MOCK.js
@@ -0,0 +1,34 @@
+const artCard_info = [{
+        id: 1,
+        techImg: "https://imgur.com/HDj2wyy.png",
+        techTitle: "Акварель",
+        techLink: "https://imgur.com/HDj2wyy.png",
+    },
+    {
+        id: 2,
+        techImg: "https://imgur.com/fC68Wpl.png",
+        techTitle: "Рисунок",
+        techLink: "https://imgur.com/fC68Wpl.png",
+    },
+    {
+        id: 3,
+        techImg: "https://imgur.com/70mHf8c.png",
+        techTitle: "Ілюстрація",
+        techLink: "https://imgur.com/70mHf8c.png",
+    },
+    {
+        id: 4,
+        techImg: "https://imgur.com/sU4BbqC.png",
+        techTitle: "Фотографія",
+        techLink: "https://imgur.com/sU4BbqC.png",
+    },
+    {
+        id: 5,
+        techImg: "https://imgur.com/GQ4q46O.png",
+        techTitle: "Історія мистецтв",
+        techLink: "https://imgur.com/GQ4q46O.png",
+    },
+
+]
+
+export default artCard_info;
