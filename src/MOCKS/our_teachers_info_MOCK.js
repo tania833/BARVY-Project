d2231b33b703f581367c0d488281c705cd0549c4
SvitlanaKalmutska <svitlachok.k@gmail.com>
2020-12-09 09:36:27 +0200

    some changes

--- src/MOCKS/our_teachers_info_MOCK.js
+++ src/MOCKS/our_teachers_info_MOCK.js
@@ -1,104 +1,104 @@
-const OurTeachers [
- {
-     id:1,
-     img: {
-         url: "https://i.imgur.com/nY2JpK0.png",
-         alt: "Khrystyna_Strynadyuk",
-     },
-     teacher: {
-         name: "Христина Стринадюк",
-         technique: "Акварель, офорт, ілюстрація",
-     },
- },
+const OurTeachers = [
+  {
+    id: 1,
+    img: {
+      url: 'https://i.imgur.com/nY2JpK0.png',
+      alt: 'Khrystyna_Strynadyuk',
+    },
+    teacher: {
+      name: 'Христина Стринадюк',
+      technique: 'Акварель, офорт, ілюстрація',
+    },
+  },
 
- {
-    id:2,
+  {
+    id: 2,
     img: {
-        url: "https://i.imgur.com/8zzEEkV.jpg",
-        alt: "Pavlo_Benedyuk",
+      url: 'https://i.imgur.com/8zzEEkV.jpg',
+      alt: 'Pavlo_Benedyuk',
     },
     teacher: {
-        name: "Павло Бенедюк",
-        technique: "Академічний рисунок, скетчинг, акварель",
+      name: 'Павло Бенедюк',
+      technique: 'Академічний рисунок, скетчинг, акварель',
     },
-},
-{
-    id:3,
+  },
+  {
+    id: 3,
     img: {
-        url: "https://i.imgur.com/fHsKJ66.png",
-        alt: "Natalia_Brichuk",
+      url: 'https://i.imgur.com/fHsKJ66.png',
+      alt: 'Natalia_Brichuk',
     },
     teacher: {
-        name: "Наталя Брічук",
-        technique: "Графіка, фотографія",
+      name: 'Наталя Брічук',
+      technique: 'Графіка, фотографія',
     },
-},
-{
-    id:4,
+  },
+  {
+    id: 4,
     img: {
-        url: "https://i.imgur.com/o7K9Wk2.png",
-        alt: "Stepan_Karpyuk",
+      url: 'https://i.imgur.com/o7K9Wk2.png',
+      alt: 'Stepan_Karpyuk',
     },
     teacher: {
-        name: "Степан Карпюк",
-        technique: "Акварель",
+      name: 'Степан Карпюк',
+      technique: 'Акварель',
     },
-},
-{
-    id:5,
+  },
+  {
+    id: 5,
     img: {
-        url: "https://i.imgur.com/ZsHo1OM.png",
-        alt: "Oleksandra_Promska",
+      url: 'https://i.imgur.com/ZsHo1OM.png',
+      alt: 'Oleksandra_Promska',
     },
     teacher: {
-        name: "Олександра Промська",
-        technique: "Ілюстрація, колаж",
+      name: 'Олександра Промська',
+      technique: 'Ілюстрація, колаж',
     },
-},
-{
-    id:6,
+  },
+  {
+    id: 6,
     img: {
-        url: "https://i.imgur.com/tRcgha5.png",
-        alt: "Natalia_Servetnyk",
+      url: 'https://i.imgur.com/tRcgha5.png',
+      alt: 'Natalia_Servetnyk',
     },
     teacher: {
-        name: "Наталя Серветник",
-        technique: "Акварель, графіка",
+      name: 'Наталя Серветник',
+      technique: 'Акварель, графіка',
     },
-},
-{
-    id:7,
+  },
+  {
+    id: 7,
     img: {
-        url: "https://i.imgur.com/GNJMt1t.png",
-        alt: "Carolina_Bundash",
+      url: 'https://i.imgur.com/GNJMt1t.png',
+      alt: 'Carolina_Bundash',
     },
     teacher: {
-        name: "Кароліна Бундаш",
-        technique: "Акварель",
+      name: 'Кароліна Бундаш',
+      technique: 'Акварель',
     },
-},
-{
-    id:8,
+  },
+  {
+    id: 8,
     img: {
-        url: "https://i.imgur.com/rYUWS6l.png",
-        alt: "Olexander_Bobryshev",
+      url: 'https://i.imgur.com/rYUWS6l.png',
+      alt: 'Olexander_Bobryshev',
     },
     teacher: {
-        name: "Олександр Бобришев",
-        technique: "Акварель",
+      name: 'Олександр Бобришев',
+      technique: 'Акварель',
     },
-},
-{
-    id:9,
+  },
+  {
+    id: 9,
     img: {
-        url: "https://i.imgur.com/FxSvvrB.png",
-        alt: "Anton_Filyk",
+      url: 'https://i.imgur.com/FxSvvrB.png',
+      alt: 'Anton_Filyk',
     },
     teacher: {
-        name: "Антон Філик",
-        technique: "Ілюстрація, скетчинг",
+      name: 'Антон Філик',
+      technique: 'Ілюстрація, скетчинг',
     },
-},  
-]
+  },
+];
 
 export default OurTeachers;
