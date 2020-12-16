aefa161e1b2351e47303dd7b0fa5dfb00be96a92
Tania Polishchuk <tania.polischuk1998@gmail.com>
2020-12-16 19:18:34 +0200

    add form validation and page adaptiveness

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -11,8 +11,8 @@ export const Contacts = () => {
       </div>
       <div className={styles.contactsContent}>
         <div className={styles.textContainer}>
-          <p className={styles.contactsPara}>+38 (096) 272-22-22</p>
-          <p className={styles.contactsPara}>barvy_courses@gmail.com</p>
+          <p className={styles.contactsPara}><a href="tel:+380962722222">+38 (096) 272-22-22</a></p>
+          <p className={styles.contactsPara}><a href="emailto:barvy_courses@gmail.com">barvy_courses@gmail.com</a></p>
           <p className={styles.contactsPara}>
             Ми на зв’язку з: <br />
             9:00 до 20:00
--- src/pages/Contacts/ContactsForm.jsx
+++ src/pages/Contacts/ContactsForm.jsx
@@ -1,25 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Contacts.module.scss';
 import { useForm } from 'react-hook-form';
-import { yupResolver } from '@hookform/resolvers/yup';
-import * as yup from 'yup';
+import { makeStyles, createStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
 
-const SignupSchema = yup.object().shape({
-  name: yup.string().required(),
-  email: yup.string().email().required(),
-});
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      fontSize: '1.5rem'
+    },
+  })
+);
 
 export const ContactsForm = () => {
-  const { register, errors } = useForm({
-    resolver: yupResolver(SignupSchema),
-  });
+  const { register } = useForm();
+  const [message, setMessage] = useState('');
 
-  const onSubmitFunc = () => {
-    console.log('submit');
+  const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const onSubmitFunc = async (event) => {
+    event.preventDefault();
+
+    const formData = new FormData();
+
+    formData.append('name', event.target.name.value);
+    formData.append('email', event.target.email.value);
+    formData.append('text', event.target.text.value);
+
+    const response = await fetch('https://dry-crag-28734.herokuapp.com', {
+      method: 'POST',
+      mode: 'cors',
+      body: formData,
+    });
+
+    if (response.status === 200) {
+      event.target.reset();
+      setMessage(
+        'Ми отримали Ваше повідомлення! Звʼяжемось найближчим часом :)'
+      );
+      setOpen(true);
+    } else {
+      setMessage('Щось пішло не так...Спробуйте ще раз :)');
+    }
   };
 
   return (
-    <form className={styles.contactsForm} onSubmit={() => onSubmitFunc()}>
+    <form
+      className={styles.contactsForm}
+      onSubmit={(event) => onSubmitFunc(event)}
+    >
       <div className={styles.contactsLabelContainer}>
         <label className={styles.contactsLabel}>Напишіть нам</label>
       </div>
@@ -28,31 +73,46 @@ export const ContactsForm = () => {
         ref={register({ maxLength: 30 })}
         placeholder="Прізвище, ім’я *"
         className={styles.contactsTextField}
+        required
       />
-      {errors.name && (
-        <p className={styles.contactsError}>{'Обовʼязкове поле'}</p>
-      )}
       <input
         name="email"
         ref={register({ min: 18, max: 99 })}
         placeholder="E-mail *"
         className={styles.contactsTextField}
+        required
+        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
       />
-      {errors.email && (
-        <p className={styles.contactsError}>
-          {'Введіть пошту в правильному форматі'}
-        </p>
-      )}
       <textarea
-        name="textarea"
+        name="text"
         className={styles.contactsTextArea}
         placeholder="Введіть Ваше повідомлення"
       />
-      <input
-        type="submit"
-        value="Відправити"
-        className={styles.contactsSubmitButton}
-      />
+      <div className={styles.contactsSubmitButtonContainer}>
+        <input
+          type="submit"
+          value="Відправити"
+          className={styles.contactsSubmitButton}
+        />
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+              <p id="transition-modal-description">{message}</p>
+            </div>
+          </Fade>
+        </Modal>
+      </div>
     </form>
   );
 };
