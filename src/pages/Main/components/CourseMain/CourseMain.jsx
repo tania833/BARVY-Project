7c33bfabae98acbe7be0a4591aaf55415f1ab923
SvitlanaKalmutska <svitlachok.k@gmail.com>
2020-12-20 23:40:39 +0200

    changes in buttons/inputs sizes and some links on buttons

--- src/pages/Main/components/CourseMain/CourseMain.jsx
+++ src/pages/Main/components/CourseMain/CourseMain.jsx
@@ -4,6 +4,7 @@ import MainTitle from '../../../../components/Title';
 import Button from '../../../../components/Button';
 import CourseCardViewerForMain from '../../../../components/CourseCard';
 import styles from './CourseMain.module.scss';
+import { Link } from 'react-router-dom';
 
 const CourseMain = () => {
   return (
@@ -13,7 +14,9 @@ const CourseMain = () => {
         card_info={card_info.filter((card, index) => index <= 5)}
       />
       <div className={styles.mainCourses_buttonWrapper}>
-        <Button text="до каталогу" />
+        <Link to="/techniques">
+          <Button text="до каталогу" />
+        </Link>
       </div>
     </section>
   );
