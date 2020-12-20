d26e1661beef292b51c22f867415ea31340fafab
Towko <antonyo0182@gmail.com>
2020-12-20 22:50:08 +0200

    added 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import Header from './components/Header/Header';
 import Main from './pages/Main/Main';
 import Artists from './pages/Artists/Artists';
 import Events from './pages/Events/Events';
+import Course from './pages/Course/Course';
 import About from './pages/About/About';
 import Techniques from './pages/Techniques/Techniques';
 import Footer from './components/Footer';
+import page404 from './pages/404/page404';
 
 function App() {
   return (
@@ -21,6 +23,8 @@ function App() {
           <Route component={Techniques} path="/techniques" />
           <Route component={Events} path="/events" />
           <Route component={About} path="/about" />
+          <Route component={Course} path="/course" />
+          <Route component={page404} path="/404" />
         </Switch>
         <Footer />
       </div>
